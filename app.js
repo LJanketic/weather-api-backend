fa3ab94a9fc7e4ececafcac4c57a57c204bce549
Luka Janketic <luka.janketic@gmail.com>
2022-07-07 16:32:05 +0200

    adding in a real cron for a test

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const cron = require('node-cron');
+const request = require('request');
 const app = express();
 
 /**
@@ -30,5 +32,27 @@ const indexRouter = require('./routes/index');
  */
 
 app.use('/weather', indexRouter);
+
+/**
+ * TEST
+ * 
+ */
+
+const options = {
+    url: 'https://api.met.no/weatherapi/locationforecast/2.0/compact.json?lat=45.8150&lon=15.9819',
+    headers: {
+        'User-Agent': 'request'
+    }
+};
+
+cron.schedule("*/15 * * * * *", function () {
+    // API call goes here
+    console.log("running a task every minute");
+    request(options, function (error, response, body) {
+          console.log('error:', error); // Print the error if one occurred and handle it
+          console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+          console.log(body)
+    });
+})
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
