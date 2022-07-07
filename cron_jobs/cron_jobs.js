acbe3d1548689f2234a070a105c71c65184189da
Luka <luka.janketic@gmail.com>
2022-07-07 19:54:26 +0200

    moved cron job to own folder, not done

--- cron_jobs/cron_jobs.js
+++ cron_jobs/cron_jobs.js
@@ -1,38 +1,14 @@
-/**
- * IMPORTS
- *
- */
+const cron = require('node-cron');
+const request = require('request');
 
-// const constConfig = require('./config.json');
-
-const express = require('express');
-const morgan = require('morgan');
-/* const cron = require('node-cron');
-const request = require('request'); */
-const app = express();
-
-/**
- * MIDDLEWARE
- *
- */
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan('tiny'));
-
-/**
- * TEST
- *
- */
-
-/* const options = {
+const options = {
     url: 'https://api.met.no/weatherapi/locationforecast/2.0/compact.json?lat=45.8150&lon=15.9819',
     headers: {
         'User-Agent': 'request'
     }
 };
 
-cron.schedule('* * * * * *', function () {
+cron.schedule('*/15 * * * * *', function () {
     // API call goes here
     console.log('CRON SUCCESSFULLY TRIGGERED');
     request(options, function (error, response, body) {
@@ -40,6 +16,4 @@ cron.schedule('* * * * * *', function () {
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
         console.log(body);
     });
-}); */
-
-app.listen(3000, () => console.log('Listening on port 3000...'));
+});
