76c72d8adcc039b3589729aee6882fe6bb3a6b81
Luka Janketic <luka.janketic@gmail.com>
2022-07-08 13:07:08 +0200

    added more cities to the config

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
-    ZAGREB_LOCATION_URL: "https://api.met.no/weatherapi/locationforecast/2.0/compact.json?lat=45.8150&lon=15.9819",
+    ZAGREB_LOCATION_URL: "https://api.met.no/weatherapi/locationforecast/2.0/compact.json?altitude=135&lat=45.8150&lon=15.9819",
+    BERLIN_LOCATION_URL: "https://api.met.no/weatherapi/locationforecast/2.0/compact.json?altitude=43&lat=52.520008&lon=13.404954",
+    BUDAPEST_LOCATION_URL: "https://api.met.no/weatherapi/locationforecast/2.0/compact.json?altitude=98&lat=47.497913&lon=19.040236",
+    PARIS_LOCATION_URL: "https://api.met.no/weatherapi/locationforecast/2.0/compact.json?altitude=42&lat=48.864716&lon=2.349014",
+    LONDON_LOCATION_URL: "https://api.met.no/weatherapi/locationforecast/2.0/compact.json?altitude=25&lat=51.4923&lon=-0.118092",
+    MOSCOW_LOCATION_URL: "https://api.met.no/weatherapi/locationforecast/2.0/compact.json?altitude=144&lat=55.751244&lon=37.618423",
     HEADERS: "request"
 };
