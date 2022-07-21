7d80ca31f6f783de8baf27e9a5c2135909122f10
Luka Janketic <luka.janketic@gmail.com>
2022-07-21 11:08:06 +0200

    went back to my old ways

--- cron_jobs/cron_jobs.js
+++ cron_jobs/cron_jobs.js
@@ -1,32 +1,24 @@
 const { response } = require('express');
 const cron = require('node-cron');
-//const request = require('request');
+const request = require('request');
 const constConfig = require('../config');
 
 
-
-
 module.exports.cityCron = async () => {
-    cron.schedule('*/15 * * * * *', async function () {
-        const url = "https://api.met.no/weatherapi/locationforecast/2.0/compact.json?altitude=135&lat=45.8150&lon=15.9819";
-        const myInit = {
-            method: 'GET',
-            headers: { 'User-Agent': constConfig.HEADERS },
-          };
-        const myRequest = new Request("https://api.met.no/weatherapi/locationforecast/2.0/compact.json?altitude=135&lat=45.8150&lon=15.9819");
-        fetch(myRequest, myInit).then((response) => {
-            if (response.status === 200) {
-              return response.json();
-            }
-            throw new Error('Something went wrong');
-          })
-          .then((responseJson) => {
-              console.log('HAHA', responseJson)
-            // Do something with the response
-          })
-          .catch((error) => {
-            console.log(error)
-          });
+    cron.schedule('*/15 * * * * *', function () {
+        // EXAMPLE CALL FOR NOW
+        constConfig.LOCATION_URLS.forEach((el) => {
+            console.log('CRON SUCCESSFULLY TRIGGERED');
+            request(el, { headers: { 'User-Agent': constConfig.HEADERS } }, function (error, response, body) {
+                if (response && response.statusCode && response.statusCode === 200) {
+                    console.log('Request code:', response && response.statusCode);
+                    console.log('REQUEST SUCCESSFUL:', body);
+                }
+                console.log('error:', error); // Print the error if one occurred and handle it
+                // Print the response status code if a response was received
+                console.log(body);
+            });
+        });
     });
 };
 
